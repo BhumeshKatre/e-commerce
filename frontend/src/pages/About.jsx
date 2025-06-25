1436d49e5c2dc779b7865b38ea3a3378d18d9b42
DevDynamoWeb <dev.bhumesh@gmail.com>
2025-06-25 07:25:47 +0530

    Refactor components and enhance cart functionality with checkout info

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,67 @@
 import React from 'react';
+import Title from '../components/Title';
+import NewsLetter from '../components/NewsLetter'
+import { assets } from '../assets/frontend_assets/assets';
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='py-5 '>
+      <Title text1={'ABOUT'} text2={'US'} />
+
+      <div className='p-5 flex gap-4'>
+        <div className=''>
+          <img
+            className=''
+            src={assets.about_img}
+            alt="" />
+        </div>
+
+        <div>
+          <p className='mb-2 text-justify '>
+            Welcome to our E-commerce platform! We are dedicated to providing you with a seamless online shopping experience, offering a wide range of quality products at competitive prices. Our mission is to ensure customer satisfaction through excellent service, fast delivery, and a user-friendly interface. Thank you for choosing us as your trusted shopping destination.
+          </p>
+          <p className='mb-2 text-justify '>
+            Our team works tirelessly to curate the best products and deals for you. We value your feedback and strive to continuously improve our services. Shop with confidence and enjoy a hassle-free experience every time you visit our site.
+          </p>
+          <p className='mb-2 text-justify '>
+            If you have any questions or need assistance, our support team is always here to help. We look forward to serving you and making your shopping journey enjoyable and rewarding!
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <Title text1={'WHY'} text2={'CHOOSE US'} />
+
+        <div className='flex py-5'>
+          <div className="flex-1 px-4">
+            <h3 className="font-semibold text-lg mb-2">Wide Selection</h3>
+            <p className="text-justify text-sm">
+              Explore a diverse range of products across multiple categories, carefully curated to meet your needs and preferences.
+            </p>
+          </div>
+          <div className="flex-1 px-4">
+            <h3 className="font-semibold text-lg mb-2">Secure Shopping</h3>
+            <p className="text-justify text-sm">
+              Shop with confidence knowing your personal information and transactions are protected with advanced security measures.
+            </p>
+          </div>
+          <div className="flex-1 px-4">
+            <h3 className="font-semibold text-lg mb-2">Fast Delivery</h3>
+            <p className="text-justify text-sm">
+              Enjoy quick and reliable delivery services, ensuring your orders reach you promptly and in perfect condition.
+            </p>
+          </div>
+          <div className="flex-1 px-4">
+            <h3 className="font-semibold text-lg mb-2">Customer Support</h3>
+            <p className="text-justify text-sm">
+              Our dedicated support team is always ready to assist you with any queries or concerns, making your experience hassle-free.
+            </p>
+          </div>
+      </div>
+      </div>
+
+      <NewsLetter />
+    </section>
   );
 }
 
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,14 +1,88 @@
-import React from 'react';
-import Title from '../components/Title'
-const Cart = () => {
-  return (
-    <section className='py-10 '>
-      <Title text1={'YOUR'} text2={'CART'} />
-      <div  className=''>
+import React from "react";
+import Title from "../components/Title";
+import { useAuth } from "../context/AuthContext";
+import { MdDelete } from "react-icons/md";
 
+const Cart = () => {
+  const { cartItem, allProducts, handleProceedBtn } = useAuth();
+  const subtotal = allProducts.slice(0,4).reduce((sum, item) => sum + item.price,0);
+  const shippingCharge = 10;
+  return (
+    <section className="py-10">
+      <Title text1={"YOUR"} text2={"CART"} />
+      <div className="px-2 sm:px-5 py-6 mt-8 border border-gray-400 rounded-md bg-white">
+        {allProducts.slice(0, 4).map((item, idx) => (
+          <div
+            key={idx}
+            className="flex flex-col w-full md:flex-row  gap-4 py-4 border-b border-gray-200"
+          >
+            {/* Product Image & Name */}
+            <div className="flex gap-4 md:w-[50%]  ">
+              <img
+                src={item.image[0]}
+                className="w-20 h-20 object-cover rounded"
+                alt=""
+              />
+              <div>
+                <p className="text-md font-semibold">{item.name}</p>
+                <p className="text-sm text-gray-500">
+                  size : {item.selectSize || "S"}
+                </p>
+              </div>
+            </div>
+
+            {/* Quantity, Price, Delete */}
+            <div className="flex flex-row md:flex-row items-center gap- md:gap-8 md:w-[50%] justify-between  ">
+              <input
+                className="w-16 border border-gray-200 text-center outline-none rounded"
+                type="number"
+                min={1}
+                defaultValue={1}
+              />
+              <div className="font-semibold">${item.price}</div>
+              <MdDelete
+                className="text-gray-500 cursor-pointer hover:text-red-500"
+                size={24}
+              />
+            </div>
+          </div>
+        ))}
       </div>
+
+      <div className="flex justify-end   p-8">
+        <div className="w-90  p-4 ">
+          <h1 className="text-xl mb-2  font-semibold ">Cart Total</h1>
+          <div>
+            <div className="flex
+               justify-between border-b border-gray-200 py-2">
+              <p className="text-sm"> Subtotal  </p>
+              <p className="font-bold">${subtotal}  </p>
+            </div>
+            <div className="flex
+               justify-between border-b border-gray-200 py-2">
+              <p className="text-sm ">Shipping Fee </p>
+              <p className="font-bold"> ${shippingCharge}  </p>
+            </div>
+            <div className="flex
+               justify-between border-b border-gray-200 py-2">
+              <p className="text-lg font-bold ">Total </p>
+              <p className="font-bold">${shippingCharge + subtotal}  </p>
+            </div>
+          </div>
+
+
+          <div className="flex justify-end py-3 ">
+            <button
+              onClick={()=>handleProceedBtn(subtotal, shippingCharge)}
+              type="submit"
+              className=" bg-black border border-gray-300 text-white px-6 py-3 hover:bg-gray-100  hover:text-black   transition">
+              Proceed to Continue
+            </button>
+          </div>
+        </div>
+      </div> 
     </section>
   );
-}
+};
 
 export default Cart;
